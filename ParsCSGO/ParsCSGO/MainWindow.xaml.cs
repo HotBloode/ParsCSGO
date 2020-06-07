ef8af91027131c4c66e11fd86caeda6d1f7d38b1
CooLe <coole_vk@mail.ru>
2020-06-07 23:33:56 +0200

    Up
    - Проверка дубликатов
    - Звуковое оповещение о добавлении новых кодов
    - Поиск кодов в отдельном потоке
    - Цикл сканера
    - Вычисления последней стр.

--- ParsCSGO/ParsCSGO/MainWindow.xaml.cs
+++ ParsCSGO/ParsCSGO/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace ParsCSGO
@@ -10,20 +12,27 @@ namespace ParsCSGO
     public partial class MainWindow : Window
     {
         Parser p = new Parser();
+        Thread myThread;
+
         public MainWindow()
         {
             InitializeComponent();            
             logList.ItemsSource = p.db;
-        }
-        
+            myThread = new Thread(new ThreadStart(p.ParsePage));
+            p.mainList = logList;              
+                        
+        }        
 
         private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            p.ParsePageAsync();           
-        }
+        {            
+            myThread.Start();
+        }       
+       
 
-        private void Button_Click1(object sender, RoutedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            var x = myThread.IsAlive;
+            int a = 0;
             logList.Items.Refresh();
         }
     }
--- ParsCSGO/ParsCSGO/Parser.cs
+++ ParsCSGO/ParsCSGO/Parser.cs
@@ -1,62 +1,82 @@
-﻿using AngleSharp.Html.Dom;
-using AngleSharp.Html.Parser;
+﻿using AngleSharp.Html.Parser;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Media;
 using System.Net.Http;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace ParsCSGO
 {
     class Parser
-    {
+    {       
+        public ListView mainList;        
         string url;
         string baseUrl = "https://miped.ru/f/threads/promokody-dlja-sajta-gocs-pro-obschaja-tema.67618/page-";
         public List<Repoz> db = new List<Repoz>();
 
+        bool globalFlag = false;
         int lastPage;
-        int startPage;
+        int startPage;      
 
         void LUrl()
         {           
             startPage = Convert.ToInt32(File.ReadAllText("Url.json"));
-        }
-
-        public async Task ParsePageAsync()
+        }        
+        
+        public async void ParsePage()
         {
-            LUrl();
-            url = baseUrl + startPage;
-            var source = await GetHtmlPage();
-            var domParser = new HtmlParser();
-            var doc = await domParser.ParseDocumentAsync(source);
-            var list = new List<string>();
-            lastPage = Convert.ToInt32(doc.QuerySelector("input[max]").Attributes[5].Value);
-
-
-            for (int i = startPage; i < lastPage; i++)
+            while (true)
             {
-                url = baseUrl + i;
-                //url = baseUrl + startPage;
-                ParseCode();
+                LUrl();
+                globalFlag = false;
+                url = baseUrl + startPage;
+                var source = await GetHtmlPage();
+                var domParser = new HtmlParser();
+                var doc = await domParser.ParseDocumentAsync(source);
+                var list = new List<string>();
+                lastPage = Convert.ToInt32(doc.QuerySelector("input[max]").Attributes[5].Value);
+
+
+                for (int i = startPage; i <= lastPage; i++)
+                {
+                    url = baseUrl + i;
+                    bool flag = await ParseCode();
+                    if (flag)
+                    {
+                        globalFlag = true;
+                    }
+                }
+
+                if (globalFlag)
+                {
+                    SystemSounds.Beep.Play();                     
+                    mainList.Dispatcher.Invoke(new Action(() => mainList.Items.Refresh()));                    
+                }                
+                //Thread.Sleep(300000);
+                File.WriteAllText("Url.json", Convert.ToString(lastPage));
+                Thread.Sleep(10000);
             }
+        }        
 
-        }
-
-         async Task<string> GetHtmlPage()
+        async Task<string> GetHtmlPage()
         {            
             string source = null;
             HttpClient client = new HttpClient();
             var respose = await client.GetAsync(url);
             if (respose != null && respose.StatusCode == System.Net.HttpStatusCode.OK)
             {
-                source = await respose.Content.ReadAsStringAsync();
+                source = await respose.Content.ReadAsStringAsync();               
             }
             return source;
         }
 
-         async Task<IHtmlDocument> ParseCode()
-        {            
+        async Task<bool> ParseCode()
+        {
+            bool flagAdd = false;
             var source = await GetHtmlPage();//Получаем HTML код
             var domParser = new HtmlParser();
             var doc = await domParser.ParseDocumentAsync(source);
@@ -80,11 +100,27 @@ namespace ParsCSGO
 
                 foreach (var match in matches)
                 {
-                    db.Add(new Repoz(match.ToString().Split('<')[0], System.DateTime.Now));
-                }
+                    if (CheckDublicate(match.ToString().Split('<')[0]))
+                    {
+                        db.Add(new Repoz(match.ToString().Split('<')[0], System.DateTime.Now));
+                        flagAdd = true;
+                    }                  
+                }                
             }
 
-            return doc;
+            return flagAdd;
+        }
+
+        bool CheckDublicate(string code)
+        {
+            for (int i = 0; i < db.Count; i++)
+            {
+                if(db[i].Code == code)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
